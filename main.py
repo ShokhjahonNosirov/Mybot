339401a85d54a891ea302a2edc41d25a59befd13
Shokhjahon <shokhjahonnosirov@gmail.com>
2022-06-17 10:17:44 +0500

    added chanhes

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from telebot import types
 bot = telebot.TeleBot('5322220961:AAHuPg46q7v9hmBGA9DEnrOIOSJn8o0jlJo')
 BOT_URL = 'https://serene-thicket-58756.herokuapp.com/'
 
-
 markup = telebot.types.ReplyKeyboardMarkup(True)
-markup.row('📦 Mahsulotlar')
+markup.row('📦 Mahsulotlar', '🎁 Aksiyalar')
 markup.row('📞 Biz bilan bog`lanish', '📢 Bizni kuzatib boring')
 
 @bot.message_handler(commands=['start'])
@@ -27,7 +26,8 @@ def send_text(message):
             p_markup.row('Bosh menyu')
 
             if message.text == '🅿️ Pompa':
-                bot.send_photo(message.chat.id, open('pompaa.jpg', 'rb'), "*Elektron pompa* \n\nNarxi: 70000.00 so'm \n\n🔻Mahsulot haqida:\n✅USB quvvatlagich\n✅ Foydalanishga qulay \n✅ Navoiy bo'ylab yetkazib berish bepul.", parse_mode='Markdown', reply_markup = p_markup) 
+                p = 'AgACAgIAAxkBAAIKXmKsCA7FDKwjS28OBETTWs4NjVgiAAJZwDEbdsZgScLNBahtZn_LAQADAgADcwADJAQ'  #pompa photo_id
+                bot.send_photo(message.chat.id, p, "*Elektron pompa* \n\nNarxi: 70000.00 so'm \n\n🔻Mahsulot haqida:\n✅USB quvvatlagich\n✅ Foydalanishga qulay \n✅ Navoiy bo'ylab yetkazib berish bepul.", parse_mode='Markdown', reply_markup = p_markup) 
             elif message.text =='🚍Buyurtma berish' or 'Ortga qaytish':
                 po_markup = telebot.types.ReplyKeyboardMarkup(True)
                 po_markup.row('Ortga qaytish')
@@ -41,7 +41,8 @@ def send_text(message):
             suv_markup.row('🚖Buyurtma berish')
             suv_markup.row('Bosh menyu')
             if message.text == '💧 Suv':
-                bot.send_photo(message.chat.id, open('suv.jpg', 'rb'), "*Shifo Suv - 18.9 L* \n\nNarxi: 10 000 so'm \n\n🔻Mahsulot haqida:\n\n💧🕋 Artezian suvi 7 qirrali maxsus filtrda koreya texnologiyasi asosida o'n ikki bosqichli maxsus filtrda tozalanib, qo'shimcha ravishda minerallashtiriladi. Minerallashtirish jarayonida inson tanasi uchun zarur bo'lgan kalsiy, ftor, yod, kaliy, magniy, natriy bilan suv tarkibi boyitiladi. Bundan tashqari, shifo suv artezian suvi filtrlardan o'tkazilib tayyor ichimlik suv holiga kelgach,  muqaddas Makka shahridan keltirilgan 'Zam-zam' suvi qo'shiladi.\n\n🚚 Navoiy bo'ylab yetkazib berish bepul.", parse_mode='Markdown', reply_markup = suv_markup) 
+                s = 'AgACAgIAAxkBAAIKYGKsCbY_tvwWHQtbPRU-HGUqk-grAAJawDEbdsZgScMW9g7oj0yvAQADAgADcwADJAQ'    # suv photo_id
+                bot.send_photo(message.chat.id, s, "*Shifo Suv - 18.9 L* \n\nNarxi: 10 000 so'm \n\n🔻Mahsulot haqida:\n\n💧🕋 Artezian suvi 7 qirrali maxsus filtrda koreya texnologiyasi asosida o'n ikki bosqichli maxsus filtrda tozalanib, qo'shimcha ravishda minerallashtiriladi. Minerallashtirish jarayonida inson tanasi uchun zarur bo'lgan kalsiy, ftor, yod, kaliy, magniy, natriy bilan suv tarkibi boyitiladi. Bundan tashqari, shifo suv artezian suvi filtrlardan o'tkazilib tayyor ichimlik suv holiga kelgach,  muqaddas Makka shahridan keltirilgan 'Zam-zam' suvi qo'shiladi.\n\n🚚 Navoiy bo'ylab yetkazib berish bepul.", parse_mode='Markdown', reply_markup = suv_markup) 
             elif message.text =='🚖Buyurtma berish' or 'Ortga qaytish':
                 order_markup = telebot.types.ReplyKeyboardMarkup(True)
                 order_markup.row('◀️Ortga qaytish')
@@ -60,6 +61,10 @@ def send_text(message):
 
     if message.text == '📢 Bizni kuzatib boring':
         bot.send_message(message.chat.id, 'Instagram: shifosuv\nFacebook: shifosuv uz\nWebsite: shifosuv.uz', reply_markup = markup)
+
+    if message.text == '🎁 Aksiyalar':
+        bot.send_message(message.chat.id, "✔️ Boshlang'ich to'lov(zalog) so'ralmaydi\n✔️ Yetkazib berish bepul", reply_markup = markup)
+
 
 bot.polling()    
 
