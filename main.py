1a0d624284615024e21b0fac3e275b853fc7c87f
Shokhjahon <shokhjahonnosirov@gmail.com>
2022-06-18 13:13:07 +0500

    did changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types 
 
 bot = telebot.TeleBot('5322220961:AAHuPg46q7v9hmBGA9DEnrOIOSJn8o0jlJo')
-BOT_URL = 'https://serene-thicket-58756.herokuapp.com/'
+BOT_URL = 'https://shifosuv.herokuapp.com/'
 
 markup = telebot.types.ReplyKeyboardMarkup(True)
 markup.row('📦 Mahsulotlar', '🎁 Aksiyalar')
